c641ebff45a1e823169408c050a161d873a2fa0e
Mesut KILINCASLAN <kilincaslanmesut@gmail.com>
2023-03-20 00:30:26 +0300

    implement vuetify

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+
+const vuetify = createVuetify({
+  components,
+});
+
+createApp(App).use(vuetify).mount("#app");
